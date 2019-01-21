5ed4578bc25d22001900d27b95bbfeb994d1246e
MarcCarranza <MarcCarranzaFerrez@gmail.com>
2019-01-21 16:30:13 +0100

    Added mobile query on css
    Signed-off-by: MarcCarranza <MarcCarranzaFerrez@gmail.com>

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Marionette from 'backbone.marionette';
 import template from '../templates/searchBar.jst';
 
@@ -23,29 +22,3 @@ let SearchBarView = Marionette.View.extend({
 let searchBar = new SearchBarView();
 
 export default searchBar;
-=======
-import Marionette from 'backbone.marionette';
-import template from '../templates/searchBar.jst';
-
-let SearchBarView = Marionette.View.extend({
-  className: 'searchbar__wrapper',
-  template: template,
-  triggers: {
-    'click #searchbar__button': 'searchClick'
-  },
-  detectEnter: function(view) {
-    $(document).on('keypress',function(e) {
-      if (e.which === 13) {
-        view.triggerMethod('searchClick', view);
-      }
-    });
-  },  
-  onRender() {
-    this.detectEnter(this);
-  }
-});
-
-let searchBar = new SearchBarView();
-
-export default searchBar;
->>>>>>> fa5562c0d5b722b230468e374508d5f30363508f
